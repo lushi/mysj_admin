e44af524c249dec6f9e87f72c1575d4e58cdd402
Lushi Li <lushili06@gmail.com>
2013-04-04 14:09:58 -0400

    make a basic Student model (including secure password)

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Student < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |s| s.email = email.downcase }
+
+  validates :name,
+  	presence: true,
+  	length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+  	presence: true,
+  	format: { with: VALID_EMAIL_REGEX },
+  	uniqueness: true
+
+  validates :password,
+  	presence: true,
+  	length: { minimum: 8 }
+
+  validates :password_confirmation,
+  	presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404161104) do
+ActiveRecord::Schema.define(:version => 20130404172144) do
 
   create_table "students", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "students", ["email"], :name => "index_students_on_email", :unique => true
 
 end
