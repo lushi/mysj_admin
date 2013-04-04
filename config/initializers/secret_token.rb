7f99630a0d9fdeb6c02e3f5b76353d20e86a76e3
Lushi Li <lushili06@gmail.com>
2013-04-04 11:19:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysjAdmin::Application.config.secret_token = 'fa6cd3aba0a4149c5d511f41aca29ab937df4634ef4112f6de5f9cba760a777dc353bc108f9afd024ac62b9acb19cab2dcbddb218fbe64d3c727f9805dea9b3f'
